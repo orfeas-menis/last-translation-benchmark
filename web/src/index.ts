0041e51de73936cee1bfb873464bd9c32aebb347
Vilém Zouhar <vilem.zouhar@gmail.com>
2026-05-04 12:28:34 +0200

    remove redirect

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,44 +1,42 @@
 import './style.css';
 import $ from 'jquery';
 
-import { getToken, getUsername, getMe } from './api';
+import { getToken, getUsername, getMe, User } from './api';
 import { setupInstructions } from './utils';
 
 $(async () => {
     setupInstructions('all');
-
-    $('#register-btn').on('click', () => {
-        window.location.href = 'profile.html';
-    });
 
     const token = getToken();
     const username = getUsername();
     if (token && username) {
         try {
             const user = await getMe();
-            showRoleButtons(user.roles);
+            showRoleButtons(user);
         } catch {
             $('#auth-error').show();
         }
     }
 });
 
-function showRoleButtons(roles: string[]): void {
+function showRoleButtons(user: User): void {
     $('#register-btn').hide();
     $('#cta-info-unauth').hide();
 
     const search = window.location.search;
     const container = $('#role-buttons');
 
-    if (roles.includes('contributor')) {
-        container.append(`<a href="contribute${search}" class="btn btn-success">✍️ Contribute</a>`);
+    container.append(`<span>Hello ${user.name}!</span><br><br>`);
+
+    if (user.roles.includes('contributor')) {
+        container.append(`<a href="contribute${search}" class="btn btn-secondary">✍️ Contribute</a>`);
     }
-    if (roles.includes('reviewer')) {
-        container.append(`<a href="review${search}" class="btn btn-success">🔍 Review</a>`);
+    if (user.roles.includes('reviewer')) {
+        container.append(`<a href="review${search}" class="btn btn-secondary">🔍 Review</a>`);
     }
-    if (roles.includes('admin')) {
-        container.append(`<a href="admin${search}" class="btn btn-success">⚙️ Admin</a>`);
+    if (user.roles.includes('admin')) {
+        container.append(`<a href="admin${search}" class="btn btn-secondary">⚙️ Admin</a>`);
     }
 
-    container.css('display', 'flex');
+    container.css('display', 'block');
 }
